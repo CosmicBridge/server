299fe51aed131acda55d527d94bbc1e14c66120b
lidarwin <darwinli.sf@gmail.com>
2018-05-02 14:06:42 -0700

    Working messages system where nodes can connect to the app and send messages to the block chain under a Username. Need to replace the index.js in shea for the global file with index_working_shea.js

---
+++ app3.js
@@ -0,0 +1,15 @@
+let shea = require('shea')
+let app = require('lotion')({
+  initialState: { messages: [] },
+  devMode: true
+})
+
+app.use((state, tx) => {
+  if (typeof tx.username === 'string' && typeof tx.message === 'string') {
+    state.messages.push({ username: tx.username, message: tx.message })
+  }
+})
+
+app.use(shea('public/'))
+
+app.listen(3000)
